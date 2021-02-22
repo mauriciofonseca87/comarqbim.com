bd74e82c63c9e71e94ff1431945f660dbe5b6eae
rafaaro <rafa@arobyte.com>
2021-02-21 23:17:24 -0500

    remove postal code billing field from checkout

--- site/web/app/themes/diefinnhutte-child/functions.php
+++ site/web/app/themes/diefinnhutte-child/functions.php
@@ -11,3 +11,18 @@ if ( ! function_exists( 'diefinnhutte_select_child_theme_enqueue_scripts' ) ) {
 	
 	add_action( 'wp_enqueue_scripts', 'diefinnhutte_select_child_theme_enqueue_scripts' );
 }
+
+/**
+* @snippet Remove the Postcode Field on the WooCommerce Checkout
+* @how-to Get CustomizeWoo.com FREE
+* @sourcecode https://businessbloomer.com/?p=461
+* @author Rodolfo Melogli
+* @testedwith WooCommerce 3.5.1
+*/
+ 
+add_filter( 'woocommerce_checkout_fields' , 'bbloomer_remove_billing_postcode_checkout' );
+ 
+function bbloomer_remove_billing_postcode_checkout( $fields ) {
+  unset($fields['billing']['billing_postcode']);
+  return $fields;
+}
